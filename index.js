6de29d7dafb7bb1c2b03a0c76de2da78287ba16a
alissonsuassuna <alissonsuassuna@gmail.com>
2020-01-06 12:50:15 -0300

    GRUD finalizado com sucesso

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.json())
+
 const users = ['Tipscode', 'Dukeza', 'Tux']
 
 server.get('/users', (req, res) =>{
@@ -13,5 +15,31 @@ server.get('/users/:index', (req, res) =>{
 
     return res.json(users[index])
 })
+
+server.post('/users', (req, res) => {
+    const { name } = req.body
+
+    users.push(name)
+
+    return res.json(users)
+})
+
+server.put('/users/:index', (req, res) => {
+    const { index } = req.params
+    const { name } = req.body
+
+    users[index] = name
+
+    return res.json(users)
+})
+
+server.delete('/users/:index', (req, res) => {
+    const { index } = req.params
+
+    users.splice(index, 1)
+
+    return res.json(users)
+})
+
 
 server.listen(3000)
